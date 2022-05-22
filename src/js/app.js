794de4b802506dabf20a4aebd8a617e25306e681
Denis Yefremenko <denvirus83@mail.ru>
2022-05-23 00:20:48 +0400

    hw-chat frontend: corrected app.js, problems with heroku

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,12 @@ const chatUI = new ChatUI();
 const chatUser = new ChatUser();
 const chatMessage = new ChatMessage();
 const chatModal = new ChatModal();
-const chatSocket = new ChatSocket('wss://chat-serb-heroku.herokuapp.com//ws');
-const chatRequest = new ChatRequest('https://chat-serb-heroku.herokuapp.com/');
+
+const chatSocket = new ChatSocket('wss://chat-backend-serb.herokuapp.com//ws');
+const chatRequest = new ChatRequest('https://chat-backend-serb.herokuapp.com/');
+
+// const chatSocket = new ChatSocket('ws://localhost:7070//ws');
+// const chatRequest = new ChatRequest('http://localhost:7070/');
 
 const сhatController = new ChatController(mainBox, chatUI, chatUser, chatMessage, chatModal, chatSocket, chatRequest);
 сhatController.control();
